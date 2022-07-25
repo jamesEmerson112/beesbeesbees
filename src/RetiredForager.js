dfb7efc17b8ed5858731d33e68d1906fdcf143eb
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-07-25 12:09:47 -0700

    Some bee implementation

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,27 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.treasureChest = [];
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  ForagerBee.prototype.forage.call(this, treasure);
+
+  // ES6
+  // super.forage();
+};
+
+var bee = new RetiredForagerBee();
+console.log(bee);
